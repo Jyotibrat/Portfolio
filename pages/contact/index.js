561260f73b8d8617aa1796968ab34ffb25310291
Jyotibrat <jyotibratb@gmail.com>
2024-12-08 00:47:16 +0530

    Fix , issues

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -88,7 +88,7 @@ const Contact = () => {
             ></textarea>
             <button className='btn rounded-full border border-white/50 max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group'>
               <span className='group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500'>
-                Let's Talk
+                Let&apos;s Talk
               </span>
               <BsArrowRight className='-translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px]' />
             </button>
