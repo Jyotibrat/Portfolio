4d21b7f22e7ea47b961b8ea657fbf3f75ae17ebe
Jyotibrat <jyotibratb@gmail.com>
2024-12-09 16:57:17 +0530

    Initial commit: Next.js project

--- components/ParticlesContainer.js
+++ components/ParticlesContainer.js
@@ -1,12 +1,12 @@
 import { Particles } from '@tsparticles/react';
 import { Engine, loadFull } from '@tsparticles/engine';
-import {React, useCallback} from 'react';
+import { React, useCallback } from 'react';
 import { color, distance } from 'framer-motion';
 
 const ParticlesContainer = () => {
   // init
   const particlesInit = useCallback(async (engine) => {
-    await loadFull(engine, true);
+    await loadFull(Engine);
   }, []);
 
   const particlesLoaded = useCallback(async () => {}, [])
